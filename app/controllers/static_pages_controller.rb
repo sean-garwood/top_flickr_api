0a84058f401cc8430fbe7ca4b6bf53927b6479c2
sean-garwood <seansgarwood@gmail.com>
2024-10-24 16:13:24 -0400

    fix: handle no results gracefully

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,16 @@
 class StaticPagesController < ApplicationController
   def index
+    return unless params[:user_id]
+
     respond_to do |format|
       format.html
       format.xml { render xml: @photos }
     end
     @flickr = Flickr.new
-    @photos = @flickr.photos.search(user_id: static_page_params[:user_id], format: "rest")
-  end
-
-  private
-
-  def static_page_params
-    params.fetch(:static_page, {}).permit(:user_id)
+    begin
+      @photos = @flickr.photos.search(user_id: params[:user_id], format: "rest")
+    rescue Flickr::FailedResponse
+      @photos = nil
+    end
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,6 +1,6 @@
 module StaticPagesHelper
   IMG_SRC_BASE_URL = "https://live.staticflickr.com/"
-  def build_img_src_url(photo)
-    IMG_SRC_BASE_URL + photo["server"] + "/" + photo["id"] + "_" + photo["secret"] + ".jpg"
+  def img_src_url(photo)
+    "#{IMG_SRC_BASE_URL}#{photo['server']}/#{photo['id']}_#{photo['secret']}.jpg"
   end
 end
